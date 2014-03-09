ee6b5bc9e09621f6bceffe6f0babbeb27b1dd87d
taxigps <taxigps@sina.com>
2014-03-09 13:32:53 +0800

    Initial code

---
+++ service.subtitles.thelastfantasy/service.py
@@ -0,0 +1,175 @@
+﻿# -*- coding: utf-8 -*-
+
+import re
+import os
+import sys
+import xbmc
+import urllib
+import xbmcvfs
+import xbmcaddon
+import xbmcgui,xbmcplugin,shutil
+from bs4 import BeautifulSoup
+
+__addon__ = xbmcaddon.Addon()
+__author__     = __addon__.getAddonInfo('author')
+__scriptid__   = __addon__.getAddonInfo('id')
+__scriptname__ = __addon__.getAddonInfo('name')
+__version__    = __addon__.getAddonInfo('version')
+__language__   = __addon__.getLocalizedString
+
+__cwd__        = xbmc.translatePath( __addon__.getAddonInfo('path') ).decode("utf-8")
+__profile__    = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode("utf-8")
+__resource__   = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) ).decode("utf-8")
+__temp__       = xbmc.translatePath( os.path.join( __profile__, 'temp') ).decode("utf-8")
+
+sys.path.append (__resource__)
+
+TLF_API = 'http://sub.eastgame.org/?s=%s'
+
+def log(module, msg):
+    xbmc.log((u"%s::%s - %s" % (__scriptname__,module,msg,)).encode('utf-8'),level=xbmc.LOGDEBUG )
+
+def normalizeString(str):
+    return str
+
+def Search( item ):
+    subtitles_list = []
+
+    log( __name__ ,"Search for [%s] by name" % (os.path.basename( item['file_original_path'] ),))
+    if item['mansearch']:
+        url = TLF_API % (item['mansearchstr'])
+    else:
+        url = TLF_API % ('%s %s' % (item['title'], item['year']))
+    socket = urllib.urlopen( url )
+    data = socket.read()
+    socket.close()
+    soup = BeautifulSoup(data)
+    results = soup.find_all(attrs={"class":"post"})
+    for it in results:
+        link = it.find_all('a')[4].get('href').encode('utf-8')
+        match = re.compile('【对应版本】(.+?)\n'.decode('utf-8')).search(it.text)
+        if match:
+            name = match.group(1).encode('utf-8')
+        else:
+            name = os.path.splitext(link.split('/')[-1])[0].split('@')[0]
+        if 'chi' in item['3let_language']:
+            subtitles_list.append({"language_name":"Chinese", "filename":name, "link":link, "language_flag":'zh', "rating":"0"})
+        if 'eng' in item['3let_language']:
+            subtitles_list.append({"language_name":"English", "filename":name, "link":link, "language_flag":'en', "rating":"0"})
+
+    if subtitles_list:
+        for it in subtitles_list:
+            listitem = xbmcgui.ListItem(label=it["language_name"],
+                                  label2=it["filename"],
+                                  iconImage=it["rating"],
+                                  thumbnailImage=it["language_flag"]
+                                  )
+
+            listitem.setProperty( "sync", "false" )
+            listitem.setProperty( "hearing_imp", "false" )
+
+            url = "plugin://%s/?action=download&link=%s&lang=%s" % (__scriptid__,
+                                                                        it["link"],
+                                                                        it["language_name"]
+                                                                        )
+            xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listitem,isFolder=False)
+
+def Download(url,lang):
+    if xbmcvfs.exists(__temp__):
+        shutil.rmtree(__temp__)
+    xbmcvfs.mkdirs(__temp__)
+
+    subtitle_list = []
+    if lang == 'Chinese':
+        if xbmc.getLanguage() == 'Chinese (Traditional)':
+            lang = 'cht'
+        else:
+            lang = 'chs'
+    else:
+        lang = 'eng'
+    exts = [".srt", ".sub", ".txt", ".smi", ".ssa", ".ass" ]
+    zipext = os.path.splitext(url.split('/')[-1])[1]
+    zip = os.path.join( __temp__, "TLF"+zipext)
+    f = urllib.urlopen(url)
+    with open(zip, "wb") as subFile:
+        subFile.write(f.read())
+    subFile.close()
+    xbmc.sleep(500)
+    xbmc.executebuiltin(('XBMC.Extract("%s","%s")' % (zip,__temp__,)).encode('utf-8'), True)
+    for subfile in xbmcvfs.listdir(zip)[1]:
+        file = os.path.join(__temp__, subfile.decode('utf-8'))
+        if (os.path.splitext( file )[1] in exts) and (subfile.split('.')[-2] == lang):
+            subtitle_list.append(file)
+
+    return subtitle_list
+
+def get_params():
+    param=[]
+    paramstring=sys.argv[2]
+    if len(paramstring)>=2:
+        params=paramstring
+        cleanedparams=params.replace('?','')
+        if (params[len(params)-1]=='/'):
+            params=params[0:len(params)-2]
+        pairsofparams=cleanedparams.split('&')
+        param={}
+        for i in range(len(pairsofparams)):
+            splitparams={}
+            splitparams=pairsofparams[i].split('=')
+            if (len(splitparams))==2:
+                param[splitparams[0]]=splitparams[1]
+
+    return param
+
+params = get_params()
+if params['action'] == 'search' or params['action'] == 'manualsearch':
+    item = {}
+    item['temp']               = False
+    item['rar']                = False
+    item['mansearch']          = False
+    item['year']               = xbmc.getInfoLabel("VideoPlayer.Year")                           # Year
+    item['season']             = str(xbmc.getInfoLabel("VideoPlayer.Season"))                    # Season
+    item['episode']            = str(xbmc.getInfoLabel("VideoPlayer.Episode"))                   # Episode
+    item['tvshow']             = normalizeString(xbmc.getInfoLabel("VideoPlayer.TVshowtitle"))   # Show
+    item['title']              = normalizeString(xbmc.getInfoLabel("VideoPlayer.OriginalTitle")) # try to get original title
+    item['file_original_path'] = urllib.unquote(xbmc.Player().getPlayingFile().decode('utf-8'))  # Full path of a playing file
+    item['3let_language']      = []
+
+    if 'searchstring' in params:
+        item['mansearch'] = True
+        item['mansearchstr'] = params['searchstring']
+
+    for lang in urllib.unquote(params['languages']).decode('utf-8').split(","):
+        item['3let_language'].append(xbmc.convertLanguage(lang,xbmc.ISO_639_2))
+
+    if item['title'] == "":
+        item['title']  = xbmc.getInfoLabel("VideoPlayer.Title")                       # no original title, get just Title
+        if item['title'] == os.path.basename(xbmc.Player().getPlayingFile()):         # get movie title and year if is filename
+            title, year = xbmc.getCleanMovieTitle(item['title'])
+            item['title'] = normalizeString(title.replace('[','').replace(']',''))
+            item['year'] = year
+
+    if item['episode'].lower().find("s") > -1:                                        # Check if season is "Special"
+        item['season'] = "0"                                                          #
+        item['episode'] = item['episode'][-1:]
+
+    if ( item['file_original_path'].find("http") > -1 ):
+        item['temp'] = True
+
+    elif ( item['file_original_path'].find("rar://") > -1 ):
+        item['rar']  = True
+        item['file_original_path'] = os.path.dirname(item['file_original_path'][6:])
+
+    elif ( item['file_original_path'].find("stack://") > -1 ):
+        stackPath = item['file_original_path'].split(" , ")
+        item['file_original_path'] = stackPath[0][8:]
+
+    Search(item)
+
+elif params['action'] == 'download':
+    subs = Download(params["link"], params["lang"])
+    for sub in subs:
+        listitem = xbmcgui.ListItem(label=sub)
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=sub,listitem=listitem,isFolder=False)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
